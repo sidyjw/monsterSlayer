a87d697a89a418276c3625e22234806130e9e3ee
Sidiney <sidineysilva1@gmail.com>
2018-12-28 15:04:05 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,15 @@
+new Vue({ 
+    el: "#app",
+    data:{
+        playerHealth: 100,
+        monsterHealth: 100,
+        isStarted: false,
+        playerDamage: null,
+        monsterDamage: null,
+    },
+    methods: {
+        attack(){
+            
+        }
+    },
+})
