3a9cb836935eab5cd529f5812ff47293f5b899cc
Sidiney <sidineysilva1@gmail.com>
2018-12-28 15:35:20 -0300

    Ja criei as funbções de ataque e cura. Porem as vezes o ataque não desconto no HP

--- main.js
+++ main.js
@@ -4,12 +4,43 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         isStarted: false,
-        playerDamage: null,
-        monsterDamage: null,
+        playerDamage: 0,
+        monsterDamage: 0,
     },
     methods: {
         attack(){
-            
+            this.playerDamage = _.random(1, 15);
+            this.monsterDamage = _.random(1, 25);
+        },
+        heal(){
+            this.playerTotalHealth += _.random(1, 30);
+            this.monsterDamage = _.random(1, 25);
         }
     },
+    computed: {
+        playerTotalHealth(){
+            return this.playerHealth -= this.monsterDamage;
+        },
+        monsterTotalHealth(){
+            return this.monsterHealth -= this.playerDamage;
+        }
+    },
+    watch: {
+        playerTotalHealth(){
+            if(this.playerTotalHealth <= 0){
+                this.isStarted = false;
+                this.playerHealth = 100;
+                this.monsterHealth = 100;
+                alert('The monster wins!');
+            }
+        },
+        monsterTotalHealth(){
+            if(this.monsterTotalHealth <= 0){
+                this.isStarted = false;
+                this.playerHealth = 100;
+                this.monsterHealth = 100;
+                alert('You wins!');
+            }
+        }
+    }
 })
