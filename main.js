5a9113eb7d21c83a7f39c39807651e60111d4973
Sidiney <sidineysilva1@gmail.com>
2018-12-28 19:40:14 -0300

    Já concertei o bug do log anterior. Foi add o specialAttack e o giveUp

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-new Vue({ 
+const app = new Vue({
     el: "#app",
-    data:{
+    data: {
         playerHealth: 100,
         monsterHealth: 100,
         isStarted: false,
@@ -8,39 +8,61 @@ new Vue({
         monsterDamage: 0,
     },
     methods: {
-        attack(){
+        reset() {
+            this.monsterDamage = 0;
+            this.playerDamage = 0;
+            this.isStarted = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        monsterAttack(){
+            this.monsterDamage = _.random(1, 25);
+            this.playerHealth -= this.monsterDamage;
+        },
+        attack() {
             this.playerDamage = _.random(1, 15);
-            this.monsterDamage = _.random(1, 25);
+            this.monsterHealth -= this.playerDamage;
+            this.monsterAttack()
         },
-        heal(){
-            this.playerTotalHealth += _.random(1, 30);
-            this.monsterDamage = _.random(1, 25);
+        heal() {
+            this.playerHealth += _.random(1, 40);
+            
+            if(this.playerHealth > 100) 
+                this.playerHealth = 100
+            
+                this.monsterAttack();
+        },
+        specialAttack(){
+            this.playerDamage = _.random(1, 45);
+            this.monsterHealth -= this.playerDamage;
+            this.monsterAttack();
+        },
+        giveUp(){
+            this.reset();
+            alert('You give up!')
         }
     },
-    computed: {
-        playerTotalHealth(){
-            return this.playerHealth -= this.monsterDamage;
-        },
-        monsterTotalHealth(){
-            return this.monsterHealth -= this.playerDamage;
-        }
-    },
+    // computed: {
+    //     playerTotalHealth(){
+    //         return this.playerHealth -= this.monsterDamage;
+    //     },
+    //     monsterTotalHealth(){
+    //         return this.monsterHealth -= this.playerDamage;
+    //     }
+    // },
     watch: {
-        playerTotalHealth(){
-            if(this.playerTotalHealth <= 0){
-                this.isStarted = false;
-                this.playerHealth = 100;
-                this.monsterHealth = 100;
+        playerHealth() {
+            if (this.playerHealth <= 0) {
+                this.reset();
                 alert('The monster wins!');
             }
         },
-        monsterTotalHealth(){
-            if(this.monsterTotalHealth <= 0){
-                this.isStarted = false;
-                this.playerHealth = 100;
-                this.monsterHealth = 100;
-                alert('You wins!');
+        monsterHealth() {
+            if (this.monsterHealth <= 0) {
+                this.reset();
+                return alert('You wins!');
             }
+            
         }
     }
 })
