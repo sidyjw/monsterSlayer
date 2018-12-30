657372cfae11285607715920f8e71ee2e4db8188
Sidiney <sidineysilva1@gmail.com>
2018-12-30 12:40:50 -0300

    Mudei o estilo da HealthBar. Está tudo ok.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = new Vue({
     el: "#app",
     data: {
         playerHealth: 100,
+        playerHealthBarColor: '#86e01e',
         monsterHealth: 100,
+        monsterHealthBarColor: '#86e01e',
         isStarted: false,
         playerDamage: 0,
         monsterDamage: 0,
@@ -51,13 +53,34 @@ const app = new Vue({
                 })
         }
     },
+    computed: {
+        playerHealthBar(){
+            return {
+                width: this.playerHealth + '%',
+                backgroundColor: this.playerHealthBarColor
+            }
+        },
+        monsterHealthBar(){
+            return {
+                width: this.monsterHealth + '%',
+                backgroundColor: this.monsterHealthBarColor
+            }
+        }
+    },
     watch: {
         playerHealth() {
             if (this.playerHealth <= 0) {
                 this.reset();
                 alert('The monster wins!');
             }
-            
+            if (this.playerHealth <= 100 && this.playerHealth > 75)
+                    return this.playerHealthBarColor = '#86e01e';
+            if (this.playerHealth <= 75 && this.playerHealth > 50)
+                    return this.playerHealthBarColor = '#f2d31b';
+            if (this.playerHealth <= 50 && this.playerHealth > 25)
+                    return this.playerHealthBarColor = '#f2b01e';
+            if (this.playerHealth <= 25)
+                    return this.playerHealthBarColor = '#f63a0f';
         },
         playerDamage() {
             this.log('log__damage--player', 'PLAYER DAMAGE', this.playerDamage)
@@ -67,6 +90,14 @@ const app = new Vue({
                 this.reset();
                 return alert('You wins!');
             }
+            if (this.monsterHealth <= 100 && this.monsterHealth > 75)
+                    return this.monsterHealthBarColor = '#86e01e';
+            if (this.monsterHealth <= 75 && this.monsterHealth > 50)
+                    return this.monsterHealthBarColor = '#f2d31b';
+            if (this.monsterHealth <= 50 && this.monsterHealth > 25)
+                    return this.monsterHealthBarColor = '#f2b01e';
+            if (this.monsterHealth <= 25)
+                    return this.monsterHealthBarColor = '#f63a0f';
         },
         monsterDamage() {
             this.log('log__damage--monster', 'MONSTER DAMAGE', this.monsterDamage)
